0b365bb6248b819f02ba69847ad738435df3df70
PJ-Onate <pedro.onate2001@alumnos.ubiobio.cl>
2025-06-30 20:52:31 -0400

    unification

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,4 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
 
 Route::post('/products', [ProductController::class, 'store']);
-
 Route::post('/register', [AuthController::class, 'register']);
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::get('/products', [ProductController::class, 'index']);
 //Route::post('/products', [ProductController::class, 'store']);
+
+Route::get('/{any}', function () {
+    return File::get(public_path('react/index.html'));
+})->where('any', '.*');
